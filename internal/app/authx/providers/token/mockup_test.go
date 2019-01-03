dde306e556560bdb326b2e016661e25fa37d2578
Carmen de Lope <cdelope@cdelope-mbp.local>
2019-01-03 09:12:56 +0100

    DeleteExpiredTokens added (TTl in scylla providers)

--- internal/app/authx/providers/token/mockup_test.go
+++ internal/app/authx/providers/token/mockup_test.go
@@ -1,8 +1,30 @@
 package token
 
-import "github.com/onsi/ginkgo"
+import (
+	"fmt"
+	"github.com/nalej/authx/internal/app/authx/entities"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	"math/rand"
+	"time"
+)
 
 var _ = ginkgo.Describe("TokenMockup", func() {
+
 	var provider = NewTokenMockup()
+
 	TokenContexts(provider)
+
+	ginkgo.It("with a register", func() {
+		for i := 0; i < 10; i++ {
+			var p = rand.Intn(10)
+			token := entities.NewTokenData(fmt.Sprintf("u%d", i), fmt.Sprintf("t%d", i), []byte("r1"), time.Now().Add(time.Duration(p)*time.Second).Unix())
+			err := provider.Add(token)
+			gomega.Expect(err).To(gomega.Succeed())
+		}
+
+		err := provider.DeleteExpiredTokens()
+		gomega.Expect(err).To(gomega.Succeed())
+
+	})
 })
