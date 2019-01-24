3f6383b9cd57bc97ed8bed19107df4d5db1e323f
Daniel Higuero <daniel@daisho.group>
2019-01-24 14:46:35 +0100

    NP-739 Add metadata helper for devices

--- pkg/interceptor/devicehelper.go
+++ pkg/interceptor/devicehelper.go
@@ -4,6 +4,48 @@
 
 package interceptor
 
+import (
+	"context"
+	"github.com/nalej/derrors"
+	"github.com/nalej/grpc-authx-go"
+	"google.golang.org/grpc/metadata"
+)
+
+const DeviceIdField = "device_id"
+const DeviceGroupIdField = "device_group_id"
+
 type DeviceRequestMetadata struct{
 	OrganizationID         string
+	DeviceGroupID string
+	DeviceID string
+	DevicePrimitive bool
+}
+
+// GetDeviceRequestMetadata extracts the request metadata from the context so that it
+// can be easily consumed by upper layers.
+func GetDeviceRequestMetadata(ctx context.Context) (*DeviceRequestMetadata, derrors.Error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, derrors.NewInvalidArgumentError("expecting JWT metadata")
+	}
+	deviceID, found := md[DeviceIdField]
+	if !found {
+		return nil, derrors.NewUnauthenticatedError("device_id not found")
+	}
+	deviceGroupID, found := md[DeviceGroupIdField]
+	if !found {
+		return nil, derrors.NewUnauthenticatedError("device_group_id not found")
+	}
+	organizationID, found := md[OrganizationIdField]
+	if !found {
+		return nil, derrors.NewUnauthenticatedError("organizationID not found")
+	}
+	_, devicePrimitive := md[grpc_authx_go.AccessPrimitive_DEVICE.String()]
+
+	return &DeviceRequestMetadata{
+		OrganizationID:  organizationID[0],
+		DeviceGroupID:   deviceGroupID[0],
+		DeviceID:        deviceID[0],
+		DevicePrimitive: devicePrimitive,
+	}, nil
 }
